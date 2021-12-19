ebcde3f372641c16f6594526f90321db3b22cd2b
Lolipopec <pacha_vlasov_95@mail.ru>
2021-12-19 19:43:02 +0300

    Дуля

--- app/src/main/java/com/example/avtorizacia/MainActivity.java
+++ app/src/main/java/com/example/avtorizacia/MainActivity.java
@@ -7,10 +7,16 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnLog, btnReg;
@@ -31,71 +37,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         etLogin = (EditText) findViewById(R.id.etLog);
         etPass = (EditText) findViewById(R.id.etPass);
 
-        dbHelper = new DBHelper(this);
-        database = dbHelper.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(DBHelper.KEY_LOGIN, "admin");
-        contentValues.put(DBHelper.KEY_PASSWORD, "admin");
-        database.insert(DBHelper.TABLE_USERS,null, contentValues);
     }
-    public void onClick(View view) {
-        Boolean f = false;
-        switch(view.getId())
-        {
+    public void onClick(View v) {
+        switch (v.getId()) {
+
             case R.id.btnLog:
-                Cursor cursor = database.query(DBHelper.TABLE_USERS, null,null,null,null,null,null);
-                Boolean check = false;
-                if(cursor.moveToFirst()){
-                    int loginIndex = cursor.getColumnIndex(DBHelper.KEY_LOGIN);
-                    int passIndex = cursor.getColumnIndex(DBHelper.KEY_PASSWORD);
-                    do{ if (etLogin.getText().toString().equals("admin") && etPass.getText().toString().equals("admin")) {
-                        if (etLogin.getText().toString().equals(cursor.getString(loginIndex)) && etPass.getText().toString().equals(cursor.getString(passIndex))) {
-                            startActivity(new Intent(this, AdminPanel.class));
-                            check = true;
-                            break;
-                        }
-                    } else if (etLogin.getText().toString().equals(cursor.getString(loginIndex)) && etPass.getText().toString().equals(cursor.getString(passIndex))) {
-                        startActivity(new Intent(this, Catalog.class));
-                        check = true;
-                        break;
-                    }
-
-
-                    }while (cursor.moveToNext());
+                if (TextUtils.isEmpty(etLogin.getText().toString()) || TextUtils.isEmpty(etPass.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Необходимо заполнить все поля", Toast.LENGTH_LONG).show();
+                } else {
+                    LoginRequest loginRequest = new LoginRequest();
+                    loginRequest.setUsername(etLogin.getText().toString());
+                    loginRequest.setPassword(etPass.getText().toString());
+                    loginUsers(loginRequest);
                 }
-                cursor.close();
-
-                if (check == false) Toast.makeText(this,"Такой пользователь не зарегестрирован", Toast.LENGTH_LONG).show();
                 break;
             case R.id.btnReg:
-                Cursor cursorRegProvLog = database.query(DBHelper.TABLE_USERS, null,null,null,null,null,null);
-
-                Boolean checkRegProvLog   = false;
-
-                if(cursorRegProvLog.moveToFirst()){
-                    int loginIndex = cursorRegProvLog.getColumnIndex(DBHelper.KEY_LOGIN);
-
-                    do{
-                        if (etLogin.getText().toString().equals(cursorRegProvLog.getString(loginIndex))) {
-                            Toast.makeText(this,"Введенный логин уже занят", Toast.LENGTH_LONG).show();
-                            checkRegProvLog = true;
-                            break;
-                        }
-
-                    }while (cursorRegProvLog.moveToNext());
-                }
-                if (checkRegProvLog == false)
-                {ContentValues contentValues = new ContentValues();
-                    contentValues.put(DBHelper.KEY_LOGIN, etLogin.getText().toString());
-                    contentValues.put(DBHelper.KEY_PASSWORD, etPass.getText().toString());
-                    database.insert(DBHelper.TABLE_USERS,null, contentValues);
-                    Toast.makeText(this,"Регистрация прошла успешно", Toast.LENGTH_SHORT).show();
-                }
-                cursorRegProvLog.close();
-
+                startActivity(new Intent(this, registr_actv.class));
                 break;
         }
 
+    }
+    public void loginUsers(LoginRequest loginRequest) {
+        Call<LoginResponse> loginResponseCall = ApiClient.getService().loginUser(loginRequest);
+        loginResponseCall.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if (response.isSuccessful()) {
+                    LoginResponse loginResponse = response.body();
+                    if (etLogin.getText().toString().equals("admin")) {
+                        startActivity(new Intent(MainActivity.this, AdminPanel.class));
+                    } else {
+                        startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    }
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Что-то пошло не так", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                String message = t.getLocalizedMessage();
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
